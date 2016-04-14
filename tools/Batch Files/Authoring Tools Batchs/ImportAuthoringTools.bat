b09874e860b5f9a1871f44380fae3f70ae12a819
Pedro Gonçalves <pedro0rsg@gmail.com>
2016-04-14 16:27:36 +0100

    - Added new batch files to the "tools" folder.   - This batch files help importing authoring tools to a folder specified in the batch

---
+++ tools/Batch Files/Authoring Tools Batchs/ImportAuthoringTools.bat
@@ -0,0 +1,36 @@
+@echo off
+set assetRepositoriesFolder=..\AssetDev
+set destination=\Authoring Tools
+SET mainExportBat=ExportAuthTools.bat
+
+::----------------------------------------------------------
+
+set targetFolder=%cd%%destination%
+set repFolder=%cd%\%assetRepositoryFolder%
+
+IF NOT EXIST "%targetFolder%" (
+	echo Target folder "%targetFolder%" does not exist
+	exit /B
+)
+
+del /q /f "%targetFolder%"
+FOR /D %%i IN ("%targetFolder%\*") DO (
+	RD /S /Q "%%i"
+)
+
+pushd %assetRepositoriesFolder%
+	echo Collecting all Authoring Tools...
+	FOR /D %%i IN (*) DO (
+		pushd %%i
+			IF EXIST "%mainExportBat%" (
+				echo Importing "%%i" Tools...
+				set d="%targetFolder%\%%i Tools"
+				MKDIR %d%
+				CALL %mainExportBat% %d%
+				echo "%%i" Tools Importing Completed
+			)
+		popd
+	)
+	echo Authoring Tools Collection Completed
+popd
+
