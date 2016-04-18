56553f07000678d68788f02ba173f911876be404
João Miguel Dias <joao.dias@gaips.inesc-id.pt>
2016-04-18 11:10:52 +0100

    - Added the EDARecognition Asset, which connects to Plux's OpenSignals app to read EDA data from a user. - Added a WindowsForm that uses the EDARecognition Asset to show on screen the EDA information read by the sensors.

---
+++ EDARecognition/MovingSampleSet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EDARecognition
+{
+    public class MovingSampleSet
+    {
+        public int WindowSize { get; private set; }
+        public float MovingAverage { get; private set; }
+        public float MovingStandardDeviation { get; private set; }
+        public float LatestSample { get; private set; }
+        public float MovingZScore { get; private set; }
+
+        private List<float> Samples;
+
+        public MovingSampleSet(int windowSize)
+        {
+            this.WindowSize = windowSize;
+            this.Samples = new List<float>(this.WindowSize);
+        }
+
+        public void AddSample(float value)
+        {
+            if (this.Samples.Count == this.WindowSize)
+            {
+                this.Samples.RemoveAt(0);
+            }
+            this.Samples.Add(value);
+            this.LatestSample = value;
+
+            this.MovingAverage = this.Samples.Average();
+
+            var aggregateDeviation = this.Samples.Aggregate((acum, s) => acum + (float)Math.Pow(s - this.MovingAverage, 2));
+            this.MovingStandardDeviation = (float)Math.Sqrt(aggregateDeviation / this.Samples.Count);
+
+            this.MovingZScore = (this.LatestSample - this.MovingAverage) / this.MovingStandardDeviation;
+        }
+    }
+}
---
+++ SpeechEmotionRecognition/SpeechEmotionRecognitionAsset.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssetPackage;
+
+//namespace SpeechEmotionRecognition
+//{
+//    public class SpeechEmotionRecognitionAsset : BaseAsset
+//    {
+//        public SpeechEmotionRecognitionAsset()
+//        {
+//        }
+
+//        public void PlaceHolderMethod()
+//        {
+//            string request = "empty";
+//            Dictionary<string, string> headers = new Dictionary<string, string>();
+
+//            headers["Authorization"] = "Basic " + System.Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes("username:password"));
+//            headers["Content-Type"] = "application/xml";
+
+//            WWW www = new WWW("https://www.l2f.inesc-id.pt/spa/services/dialogAct", System.Text.Encoding.ASCII.GetBytes(request), headers);
+//            yield return www;
+//            if (www.error != null)
+//            {
+//                yield break;
+//            }
+
+//            string o = www.text;
+//            o = o.Remove(0, o.IndexOf("<result>") + 8);
+//            o = o.Remove(o.IndexOf("</result>"), o.Length - o.IndexOf("</result>") - 1);
+            
+//        }
+//    }
+//}
