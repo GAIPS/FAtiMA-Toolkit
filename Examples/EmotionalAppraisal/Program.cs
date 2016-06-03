9c9b624fe03ea01cd608e9c7bd7a79ab9b9e6642
Samuel Mascarenhas <samuel.fm@gmail.com>
2016-06-03 20:14:22 +0100

    Small Reference Fix

---
+++ Examples/EmotionalAppraisal/Program.cs
@@ -0,0 +1,12 @@
+﻿using EmotionalAppraisal;
+
+namespace EmotionalAppraisal2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmotionalAppraisalAsset ea = new EmotionalAppraisalAsset("Client");
+        }
+    }
+}
