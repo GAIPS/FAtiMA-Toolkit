8ad5fe335b681e1a2fcd7011bf005537b2ac31ef
Samuel Mascarenhas <samuel.fm@gmail.com>
2016-07-26 16:10:44 +0100

    Small reference fix; Changed the SI Tutorial

--- Tutorials/SocialImportanceTutorial/Program.cs
+++ Tutorials/SocialImportanceTutorial/Program.cs
@@ -11,6 +11,8 @@ namespace SocialImportanceTutorial
         //This is a small console program to exemplify the main functionality of the Social Importance Asset
         static void Main(string[] args)
         {
+            var siTarget = "Player";
+
             Console.WriteLine(Directory.GetCurrentDirectory());
             //First, we load the asset from an existing profile
             var siAsset = SocialImportanceAsset.LoadFromFile(LocalStorageProvider.Instance, "../../../Examples/SITest.si");
@@ -19,7 +21,9 @@ namespace SocialImportanceTutorial
             var ea = EmotionalAppraisalAsset.LoadFromFile(LocalStorageProvider.Instance, "../../../Examples/EATest.ea");
             siAsset.BindEmotionalAppraisalAsset(ea);
             
-            Console.WriteLine("The SI attributed to the player is:" + siAsset.GetSocialImportance("Player"));
+            Console.WriteLine("The SI attributed to "+siTarget+" is:" + siAsset.GetSocialImportance(siTarget));
+
+            Console.WriteLine("Conferral to execute: " + siAsset.DecideConferral("SELF")?.ActionName);
 
             Console.ReadKey();
         }
