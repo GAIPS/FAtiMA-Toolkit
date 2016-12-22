991814c1b3c33e74e2694e8d53bb15a97f2996b6
Samuel Mascarenhas <samuel.fm@gmail.com>
2016-12-22 14:52:41 +0000

    merge fix

--- Components/Utilities/Util.cs
+++ Components/Utilities/Util.cs
@@ -8,5 +8,12 @@
 			v1 = v2;
 			v2 = aux;
 		}
-	}
+
+        //Extension Method
+        public static bool EqualsIgnoreCase(this string str, string str2)
+        {
+            return string.Equals(str, str2, System.StringComparison.CurrentCultureIgnoreCase);
+        }
+
+    }
 }
