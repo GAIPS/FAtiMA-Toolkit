2550121e57245502243d4d238e44d09c03de2980
Samuel Mascarenhas <samuel.fm@gmail.com>
2017-05-11 11:56:41 +0100

    Finalized the testing of the new GUI for the IAT; This new version of the GUI has the Dialogue Editor integrated directly in the main form;

--- GUI/IntegratedAuthoringToolWF/MainForm.cs
+++ GUI/IntegratedAuthoringToolWF/MainForm.cs
@@ -9,6 +9,12 @@ using IntegratedAuthoringTool;
 using IntegratedAuthoringTool.DTOs;
 using WellFormedNames;
 using RolePlayCharacter;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System.IO;
+using Utilities;
+using Utilities.DataStructures;
+using System.Drawing;
 
 namespace IntegratedAuthoringToolWF
 {
@@ -24,8 +30,6 @@ namespace IntegratedAuthoringToolWF
 
 		private Dictionary<string,Form> _openedForms = new Dictionary<string, Form>();
 
-        private int stateCounter;
-        private int totalSize;
 
         public MainForm()
 		{
@@ -33,21 +37,7 @@ namespace IntegratedAuthoringToolWF
 			buttonEditCharacter.Enabled = false;
 			buttonRemoveCharacter.Enabled = false;
         }
-
-
-        public void InitializeDialogueForm()
-        {
-            _playerDialogs = new BindingListView<GUIDialogStateAction>(new List<GUIDialogStateAction>());
-            dataGridViewPlayerDialogueActions.DataSource = _playerDialogs;
-            RefreshPlayerDialogs();
-
-            _agentDialogs = new BindingListView<GUIDialogStateAction>(new List<GUIDialogStateAction>());
-            dataGridViewAgentDialogueActions.DataSource = _agentDialogs;
-            RefreshAgentDialogs();
-            stateCounter = 0;
-            totalSize = 0;
-        }
-
+        
 
         private void RefreshPlayerDialogs()
         {
@@ -66,19 +56,21 @@ namespace IntegratedAuthoringToolWF
             dataGridViewAgentDialogueActions.Columns["Id"].Visible = false;
         }
 
-        private void buttonAddPlayerDialogueAction_Click(object sender, EventArgs e)
-        {
-            new AddOrEditDialogueActionForm(this, true).ShowDialog();
-            RefreshPlayerDialogs();
-        }
-
+       
         protected override void OnAssetDataLoaded(IntegratedAuthoringToolAsset asset)
 		{
 			textBoxScenarioName.Text = asset.ScenarioName;
 			textBoxScenarioDescription.Text = asset.ScenarioDescription;
 			_characterSources = new BindingListView<CharacterSourceDTO>(asset.GetAllCharacterSources().ToList());
 			dataGridViewCharacters.DataSource = _characterSources;
-		}
+            _playerDialogs = new BindingListView<GUIDialogStateAction>(new List<GUIDialogStateAction>());
+            dataGridViewPlayerDialogueActions.DataSource = _playerDialogs;
+            RefreshPlayerDialogs();
+
+            _agentDialogs = new BindingListView<GUIDialogStateAction>(new List<GUIDialogStateAction>());
+            dataGridViewAgentDialogueActions.DataSource = _agentDialogs;
+            RefreshAgentDialogs();
+        }
 
 		private void buttonCreateCharacter_Click(object sender, EventArgs e)
 		{
@@ -172,26 +164,6 @@ namespace IntegratedAuthoringToolWF
 			}
 		}
 
-		#region Toolbar Options
-
-		private bool _dialogEditorIsShowing;
-
-		[MenuItem("Tools/Show Dialog Editor #ctrl+D")]
-		private void ShowToolbar()
-		{
-			var d = new DialogueEditorForm(this);
-			_dialogEditorIsShowing = true;
-			d.Closed += (sender, args) => { _dialogEditorIsShowing = false; };
-			d.Show();
-		}
-
-		[MenuItem("Tools/Show Dialog Editor")]
-		private bool ShowToolbar_Validation()
-		{
-			return !_dialogEditorIsShowing;
-		}
-		#endregion
-
 		#region About
 
 		[MenuItem("About",Priority = int.MaxValue)]
@@ -235,12 +207,31 @@ namespace IntegratedAuthoringToolWF
 
         private void buttonPlayerEditDialogueAction_Click(object sender, EventArgs e)
         {
-
+            if (dataGridViewPlayerDialogueActions.SelectedRows.Count == 1)
+            {
+                var item = ((ObjectView<GUIDialogStateAction>)dataGridViewPlayerDialogueActions.SelectedRows[0].DataBoundItem).Object;
+                new AddOrEditDialogueActionForm(this, true, item.Id).ShowDialog();
+                RefreshPlayerDialogs();
+            }
         }
 
         private void buttonPlayerDuplicateDialogueAction_Click(object sender, EventArgs e)
         {
+            if (dataGridViewPlayerDialogueActions.SelectedRows.Count == 1)
+            {
+                var item = ((ObjectView<GUIDialogStateAction>)dataGridViewPlayerDialogueActions.SelectedRows[0].DataBoundItem).Object;
 
+                var newDialogueAction = new DialogueStateActionDTO
+                {
+                    CurrentState = item.CurrentState,
+                    NextState = item.NextState,
+                    Meaning = item.Meaning.Split(',').Where(s => !string.IsNullOrEmpty(s)).ToArray(),
+                    Style = item.Style.Split(',').Where(s => !string.IsNullOrEmpty(s)).ToArray(),
+                    Utterance = item.Utterance
+                };
+                LoadedAsset.AddPlayerDialogAction(newDialogueAction);
+                RefreshPlayerDialogs();
+            }
         }
 
         private void buttonPlayerRemoveDialogueAction_Click(object sender, EventArgs e)
@@ -258,42 +249,302 @@ namespace IntegratedAuthoringToolWF
 
         private void buttonAgentEditDialogAction_Click(object sender, EventArgs e)
         {
-
+            if (dataGridViewAgentDialogueActions.SelectedRows.Count == 1)
+            {
+                var item = ((ObjectView<GUIDialogStateAction>)dataGridViewAgentDialogueActions.SelectedRows[0].DataBoundItem).Object;
+                new AddOrEditDialogueActionForm(this, false, item.Id).ShowDialog();
+                RefreshAgentDialogs();
+            }
         }
 
         private void buttonAgentDuplicateDialogueAction_Click(object sender, EventArgs e)
         {
+             if (dataGridViewAgentDialogueActions.SelectedRows.Count == 1)
+            {
+                var item = ((ObjectView<GUIDialogStateAction>)dataGridViewAgentDialogueActions.SelectedRows[0].DataBoundItem).Object;
 
+                var newDialogueAction = new DialogueStateActionDTO
+                {
+                    CurrentState = item.CurrentState,
+                    NextState = item.NextState,
+                    Meaning = item.Meaning.Split(',').Where(s => !string.IsNullOrEmpty(s)).ToArray(),
+                    Style = item.Style.Split(',').Where(s => !string.IsNullOrEmpty(s)).ToArray(),
+                    Utterance = item.Utterance
+                };
+                LoadedAsset.AddAgentDialogAction(newDialogueAction);
+                RefreshAgentDialogs();
+            }
         }
 
         private void buttonAgentRemoveDialogAction_Click(object sender, EventArgs e)
         {
-
+            IList<Guid> itemsToRemove = new List<Guid>();
+            for (int i = 0; i < dataGridViewAgentDialogueActions.SelectedRows.Count; i++)
+            {
+                var item = ((ObjectView<GUIDialogStateAction>)dataGridViewAgentDialogueActions.SelectedRows[i].DataBoundItem).Object;
+                itemsToRemove.Add(item.Id);
+            }
+            LoadedAsset.RemoveDialogueActions(AGENT, itemsToRemove);
+            RefreshAgentDialogs();
+            SetModified();
         }
 
         private void buttonImportExcel_Click(object sender, EventArgs e)
         {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Excel Workbook|*.xlsx";
+            if (ofd.ShowDialog() != DialogResult.OK)
+                return;
 
+            var fileName = new FileInfo(ofd.FileName);
+            using (var excelDoc = new ExcelPackage(fileName))
+            {
+                var playerDialogs = ImportWorkSheet(excelDoc, "Player Dialogs").ToArray();
+                var agentDialogs = ImportWorkSheet(excelDoc, "Agent Dialogs").ToArray();
+
+                //Clear all actions
+                LoadedAsset.RemoveDialogueActions(PLAYER, LoadedAsset.GetDialogueActionsBySpeaker(PLAYER).ToArray());
+                LoadedAsset.RemoveDialogueActions(AGENT, LoadedAsset.GetDialogueActionsBySpeaker(PLAYER).ToArray());
+                
+                foreach (var d in playerDialogs)
+                    LoadedAsset.AddPlayerDialogAction(d);
+
+                foreach (var d in agentDialogs)
+                    LoadedAsset.AddAgentDialogAction(d);
+            }
+
+            RefreshPlayerDialogs();
+            RefreshAgentDialogs();
+        }
+
+        private static IEnumerable<DialogueStateActionDTO> ImportWorkSheet(ExcelPackage package, string workSheetName)
+        {
+            var worksheet = package.Workbook.Worksheets[workSheetName];
+            if (worksheet == null)
+                throw new Exception($"Could not find worksheet with the name \"{workSheetName}\"");
+
+            var lastRow = worksheet.Dimension.End.Row;
+            var cells = worksheet.Cells;
+            for (int i = 3; i <= lastRow; i++)
+            {
+                var rowValues = Enumerable.Range(1, 5).Select(j => cells[i, j].Text).ToArray();
+                if (rowValues.All(string.IsNullOrEmpty))
+                    continue;
+
+                var value = new DialogueStateActionDTO()
+                {
+                    CurrentState = rowValues[0],
+                    NextState = rowValues[1],
+                    Utterance = rowValues[2],
+                    Meaning = rowValues[3].Split(',').Select(s => s.Trim()).Where(s => !string.IsNullOrEmpty(s)).ToArray(),
+                    Style = rowValues[4].Split(',').Select(s => s.Trim()).Where(s => !string.IsNullOrEmpty(s)).ToArray()
+                };
+
+                yield return value;
+            }
         }
 
         private void buttonExportExcel_Click(object sender, EventArgs e)
         {
+            var sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Workbook|*.xlsx";
+            if (sfd.ShowDialog() != DialogResult.OK)
+                return;
 
+            var fileName = new FileInfo(sfd.FileName);
+            using (var excelDoc = new ExcelPackage())
+            {
+                ExportWorkSheet(excelDoc, "Player Dialogs", LoadedAsset.GetDialogueActionsBySpeaker(PLAYER));
+                ExportWorkSheet(excelDoc, "Agent Dialogs", LoadedAsset.GetDialogueActionsBySpeaker(AGENT));
+                excelDoc.SaveAs(fileName);
+            }
         }
+
+
+        private static void ExportWorkSheet(ExcelPackage package, string workSheetName, IEnumerable<DialogueStateActionDTO> dialogActions)
+        {
+            var worksheet = package.Workbook.Worksheets.Add(workSheetName);
+
+            //Worksheet Header
+            {
+                var header = worksheet.Cells[1, 1, 1, 5];
+                header.Merge = true;
+                header.Value = workSheetName;
+                header.Style.Border.BorderAround(ExcelBorderStyle.Thick);
+                header.Style.Font.Bold = true;
+                header.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                header.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                header.Style.Font.Color.SetColor(Color.White);
+                header.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                header.Style.Fill.BackgroundColor.SetColor(Color.Black);
+            }
+
+            //Column Headers
+            {
+                var header = worksheet.Cells[2, 1, 2, 5];
+                header.Style.Font.Bold = true;
+                var border = header.Style.Border;
+                border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+                header.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                header.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+                worksheet.Cells[2, 1].Value = "Current State";
+                worksheet.Cells[2, 2].Value = "Next State";
+                worksheet.Cells[2, 3].Value = "Utterance";
+                worksheet.Cells[2, 4].Value = "Meanings";
+                worksheet.Cells[2, 5].Value = "Styles";
+            }
+
+            worksheet.View.FreezePanes(3, 1);
+
+            var cellIndex = 3;
+            foreach (var d in dialogActions)
+            {
+                var line = worksheet.Cells[cellIndex, 1, cellIndex, 5];
+                var border = line.Style.Border;
+                border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+                line.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+
+                worksheet.Cells[cellIndex, 1].Value = d.CurrentState;
+                worksheet.Cells[cellIndex, 2].Value = d.NextState;
+                worksheet.Cells[cellIndex, 3].Value = d.Utterance;
+                worksheet.Cells[cellIndex, 4].Value = d.Meaning.AggregateToString(", ");
+                worksheet.Cells[cellIndex, 5].Value = d.Style.AggregateToString(", ");
+
+                cellIndex++;
+            }
+
+            worksheet.Cells[2, 1, cellIndex, 5].AutoFilter = true;
+
+            for (int i = 1; i < 6; i++)
+            {
+                worksheet.Column(i).AutoFit();
+            }
+        }
+
 
         private void buttonImportTxt_Click(object sender, EventArgs e)
         {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Text File|*.txt";
+            if (ofd.ShowDialog() != DialogResult.OK)
+                return;
 
+            var fileName = new FileInfo(ofd.FileName);
+            File.SetAttributes(fileName.DirectoryName, FileAttributes.Normal);
+
+            LoadedAsset.RemoveDialogueActions(PLAYER, LoadedAsset.GetDialogueActionsBySpeaker(PLAYER).ToArray());
+            LoadedAsset.RemoveDialogueActions(AGENT, LoadedAsset.GetDialogueActionsBySpeaker(PLAYER).ToArray());
+
+            int stateCounter = 0;
+            var lines = File.ReadAllLines(fileName.FullName);
+            var totalSize = lines.Length;
+
+            foreach (var line in lines)
+            {
+                if (line.Contains("P:"))
+                {
+                    var auxLine = line.Replace("P:", "");
+                    var add = GenerateDialogueActionFromLine(auxLine, totalSize, ref stateCounter);
+                    LoadedAsset.AddPlayerDialogAction(add);
+                }
+                else if (line.Contains("A:"))
+                {
+                    var auxLine = line.Replace("A:", "");
+                    var add = GenerateDialogueActionFromLine(auxLine, totalSize, ref stateCounter);
+                    LoadedAsset.AddAgentDialogAction(add);
+
+                }
+            }
+
+            RefreshPlayerDialogs();
+            RefreshAgentDialogs();
+        }
+
+        private DialogueStateActionDTO GenerateDialogueActionFromLine(string line, int totalSize, ref int stateCounter)
+        {
+            line = line.Replace("A:", "");
+            char[] delimitedchars = { '\n' };
+            line = line.Trim();
+
+            var result = line.Split(delimitedchars);
+            var currentState = "";
+            var nextState = "";
+            
+            if (stateCounter == 0)
+            {
+                currentState = IATConsts.INITIAL_DIALOGUE_STATE;
+                stateCounter += 1;
+                nextState = "S" + stateCounter;
+            }
+            else
+            {
+                currentState = "S" + stateCounter;
+                stateCounter += 1;
+                nextState = "S" + stateCounter;
+            }
+            if (stateCounter == totalSize)
+                nextState = "End";
+
+            var add = new DialogueStateActionDTO()
+            {
+                CurrentState = currentState,
+                Meaning = new string[1],
+                NextState = nextState,
+                Utterance = result[0],
+                Style = new string[1]
+
+            };
+
+            return add;
         }
 
         private void buttonTTS_Click(object sender, EventArgs e)
         {
-
+            var dialogs = LoadedAsset.GetDialogueActionsBySpeaker(AGENT).ToArray();
+            var t = new TextToSpeechForm(dialogs);
+            t.Show(this);
         }
 
         private void buttonValidate_Click(object sender, EventArgs e)
         {
+            var dfsearch = new DFSearch<string>(state => LoadedAsset.GetAllDialogueActionsByState(state).Select(dto => dto.NextState));
+            dfsearch.InitializeSearch(IATConsts.INITIAL_DIALOGUE_STATE);
+            dfsearch.FullSearch();
 
+            int unreachableStatesCount = 0;
+            int totalStates = 0;
+            string unreachableStatesDescription = "The following Dialogue States are not reachable: \n[";
+
+            foreach (var dAction in LoadedAsset.GetAllDialogueActions().GroupBy(da => da.CurrentState).Select(group => group.First()))
+            {
+                totalStates++;
+                if (dfsearch.Closed.SearchInClosed(new NodeRecord<string>() { node = dAction.CurrentState.ToString() }) == null)
+                {
+                    unreachableStatesCount++;
+                    unreachableStatesDescription += dAction.CurrentState + ", ";
+                }
+            }
+
+            unreachableStatesDescription = unreachableStatesDescription.Remove(unreachableStatesDescription.Length - 2);
+            unreachableStatesDescription += "]";
+
+
+            string validationMessage;
+
+            if (unreachableStatesCount > 0)
+            {
+                validationMessage = "Reachability: " + (totalStates - unreachableStatesCount) * 100 / totalStates + "%\n" + unreachableStatesDescription;
+            }
+            else
+            {
+                validationMessage = "All Dialogue States are reachable!";
+            }
+
+            //get the dead ends
+            validationMessage += "\n\nEnd States:\n[" + string.Join(",", dfsearch.End.ToArray()) + "]";
+
+            MessageBox.Show(validationMessage);
         }
     }
 }
--- GUI/IntegratedAuthoringToolWF/Program.cs
+++ GUI/IntegratedAuthoringToolWF/Program.cs
@@ -20,8 +20,7 @@ namespace IntegratedAuthoringToolWF
 
 	        var form = new MainForm();
 			form.CreateNewAsset();
-            form.InitializeDialogueForm();
-			Application.Run(form);
+            Application.Run(form);
         }
     }
 }
