eb30bf4127546de7434885242578dacfbc6b2a51
Samuel Mascarenhas <samuel.fm@gmail.com>
2017-08-04 14:30:13 +0100

    Removed the AuthorialAgentsWF Project given that it was discontinued

--- AuthoringTools/EmotionalDecisionMakingWF/MainForm.cs
+++ AuthoringTools/EmotionalDecisionMakingWF/MainForm.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Windows.Forms;
 using EmotionalDecisionMaking;
 using EmotionalDecisionMaking.DTOs;
-using EmotionalDecisionMakingWF.Properties;
 using Equin.ApplicationFramework;
 using GAIPS.AssetEditorTools;
 
