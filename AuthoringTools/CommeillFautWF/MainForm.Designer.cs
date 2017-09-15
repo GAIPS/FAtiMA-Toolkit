1bb07106ce0ff2098fcaeed6d524db0d1f2d8407
Manuel Guimaraes <manuel_guimaraes@hotmail.com>
2017-09-15 15:07:47 +0100

    CiF-UI Getting prettier everyday PS: There's something about BindingListViews that I looove

--- AuthoringTools/CommeillFautWF/MainForm.Designer.cs
+++ AuthoringTools/CommeillFautWF/MainForm.Designer.cs
@@ -31,13 +31,6 @@
             this.components = new System.ComponentModel.Container();
             this.socialExchangeBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.TabControl = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.actionNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.intentDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.EditSE = new System.Windows.Forms.Button();
-            this.RemoveSE = new System.Windows.Forms.Button();
-            this.AddSE = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.triggerRulesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -45,13 +38,14 @@
             this.EditTriggerRules = new System.Windows.Forms.Button();
             this.RemoveTriggerRules = new System.Windows.Forms.Button();
             this.AddTriggerRule = new System.Windows.Forms.Button();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.genericPropertyDataGridControler1 = new GAIPS.AssetEditorTools.GenericPropertyDataGridControler();
             ((System.ComponentModel.ISupportInitialize)(this.socialExchangeBindingSource)).BeginInit();
             this.TabControl.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.triggerRulesBindingSource)).BeginInit();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // socialExchangeBindingSource
@@ -72,84 +66,6 @@
             this.TabControl.Size = new System.Drawing.Size(859, 455);
             this.TabControl.TabIndex = 15;
             // 
-            // tabPage1
-            // 
-            this.tabPage1.Controls.Add(this.dataGridView1);
-            this.tabPage1.Controls.Add(this.EditSE);
-            this.tabPage1.Controls.Add(this.RemoveSE);
-            this.tabPage1.Controls.Add(this.AddSE);
-            this.tabPage1.Location = new System.Drawing.Point(4, 29);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(851, 422);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Social Exchange";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.actionNameDataGridViewTextBoxColumn,
-            this.intentDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.socialExchangeBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(23, 75);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(822, 341);
-            this.dataGridView1.TabIndex = 11;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
-            // 
-            // actionNameDataGridViewTextBoxColumn
-            // 
-            this.actionNameDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.actionNameDataGridViewTextBoxColumn.DataPropertyName = "ActionName";
-            this.actionNameDataGridViewTextBoxColumn.FillWeight = 73.09644F;
-            this.actionNameDataGridViewTextBoxColumn.HeaderText = "ActionName";
-            this.actionNameDataGridViewTextBoxColumn.Name = "actionNameDataGridViewTextBoxColumn";
-            this.actionNameDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // intentDataGridViewTextBoxColumn
-            // 
-            this.intentDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.intentDataGridViewTextBoxColumn.DataPropertyName = "Intent";
-            this.intentDataGridViewTextBoxColumn.FillWeight = 126.9036F;
-            this.intentDataGridViewTextBoxColumn.HeaderText = "Intent";
-            this.intentDataGridViewTextBoxColumn.Name = "intentDataGridViewTextBoxColumn";
-            // 
-            // EditSE
-            // 
-            this.EditSE.Location = new System.Drawing.Point(144, 17);
-            this.EditSE.Name = "EditSE";
-            this.EditSE.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.EditSE.Size = new System.Drawing.Size(101, 39);
-            this.EditSE.TabIndex = 14;
-            this.EditSE.Text = "Edit";
-            this.EditSE.UseVisualStyleBackColor = true;
-            this.EditSE.Click += new System.EventHandler(this.EditClick);
-            // 
-            // RemoveSE
-            // 
-            this.RemoveSE.Location = new System.Drawing.Point(260, 17);
-            this.RemoveSE.Name = "RemoveSE";
-            this.RemoveSE.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.RemoveSE.Size = new System.Drawing.Size(106, 39);
-            this.RemoveSE.TabIndex = 13;
-            this.RemoveSE.Text = "Remove";
-            this.RemoveSE.UseVisualStyleBackColor = true;
-            this.RemoveSE.Click += new System.EventHandler(this.RemoveClick);
-            // 
-            // AddSE
-            // 
-            this.AddSE.Location = new System.Drawing.Point(23, 17);
-            this.AddSE.Name = "AddSE";
-            this.AddSE.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.AddSE.Size = new System.Drawing.Size(101, 39);
-            this.AddSE.TabIndex = 12;
-            this.AddSE.Text = "Add";
-            this.AddSE.UseVisualStyleBackColor = true;
-            this.AddSE.Click += new System.EventHandler(this.AddClick);
-            // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.dataGridView2);
@@ -159,7 +75,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1060, 354);
+            this.tabPage2.Size = new System.Drawing.Size(851, 422);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Trigger Rules";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -225,6 +141,30 @@
             this.AddTriggerRule.UseVisualStyleBackColor = true;
             this.AddTriggerRule.Click += new System.EventHandler(this.AddTriggerRule_Click);
             // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.genericPropertyDataGridControler1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(851, 422);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Social Exchange";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // genericPropertyDataGridControler1
+            // 
+            this.genericPropertyDataGridControler1.AllowMuliSelect = false;
+            this.genericPropertyDataGridControler1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.genericPropertyDataGridControler1.Location = new System.Drawing.Point(-4, 3);
+            this.genericPropertyDataGridControler1.Margin = new System.Windows.Forms.Padding(0);
+            this.genericPropertyDataGridControler1.Name = "genericPropertyDataGridControler1";
+            this.genericPropertyDataGridControler1.Size = new System.Drawing.Size(855, 358);
+            this.genericPropertyDataGridControler1.TabIndex = 0;
+            this.genericPropertyDataGridControler1.Load += new System.EventHandler(this.genericPropertyDataGridControler1_Load_1);
+            // 
             // MainForm
             // 
             this.AllowDrop = true;
@@ -240,11 +180,10 @@
             this.Controls.SetChildIndex(this.TabControl, 0);
             ((System.ComponentModel.ISupportInitialize)(this.socialExchangeBindingSource)).EndInit();
             this.TabControl.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.triggerRulesBindingSource)).EndInit();
+            this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -253,11 +192,6 @@
         #endregion
         private System.Windows.Forms.BindingSource socialExchangeBindingSource;
         private System.Windows.Forms.TabControl TabControl;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button EditSE;
-        private System.Windows.Forms.Button RemoveSE;
-        private System.Windows.Forms.Button AddSE;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.BindingSource triggerRulesBindingSource;
@@ -265,8 +199,8 @@
         private System.Windows.Forms.Button RemoveTriggerRules;
         private System.Windows.Forms.Button AddTriggerRule;
         private System.Windows.Forms.DataGridViewTextBoxColumn triggerRulesDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn actionNameDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn intentDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TabPage tabPage1;
+        private GAIPS.AssetEditorTools.GenericPropertyDataGridControler genericPropertyDataGridControler1;
     }
 }
 
