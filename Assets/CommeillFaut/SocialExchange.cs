cfe35ad772a15d5c63eab3c5a571b6a8878ebc43
Manuel Guimaraes <manuel_guimaraes@hotmail.com>
2018-01-12 14:50:51 +0000

    More changes to the CiF Asset

--- Assets/CommeillFaut/SocialExchange.cs
+++ Assets/CommeillFaut/SocialExchange.cs
@@ -96,22 +96,39 @@ namespace CommeillFaut
             float toRet = 0.0f;
             var totalCertainty = 0.0f;
             int totalConds = Conditions.Count();
-            var toEvaluate = Conditions;
-            var sub = new Substitution(Name.BuildName(Target), new ComplexValue(Name.BuildName(targ)));
-            var eval = toEvaluate.Unify(m_Kb, Name.BuildName(init), new[] { new SubstitutionSet(sub) });
-            if (eval.Any())
+
+            var targetSub = new Substitution(Name.BuildName(Target), new ComplexValue(Name.BuildName(targ)));
+            var initiatorSub = new Substitution(Name.BuildName(Target), new ComplexValue(Name.BuildName(init)));
+
+            var constraints = new SubstitutionSet();
+            constraints.AddSubstitution(targetSub);
+            constraints.AddSubstitution(initiatorSub);
+
+
+            foreach (var c in Conditions) // For instance SI([x]) >= 40
             {
-                var cond = "";
-                var toAsk = new string[] { };
-                foreach (var c in toEvaluate)
+
+               var resultingConstraints = c.Unify(m_Kb, init, new[] { constraints } );  // Whats the sub here [x]/John
+
+                var total = 0.0f;
+                var totalSets = resultingConstraints.Count();
+                foreach (var res in resultingConstraints)
                 {
-                    cond = c.ToString();
-                    toAsk = cond.Split(')');
-                    toAsk[0] += ")";
-               //     toAsk[0] = toAsk[0].Replace("[x]", targ);
-                    var certainty = m_Kb.AskProperty(Name.BuildName(toAsk[0])).Certainty;
-                    totalCertainty += certainty;
+                    if (resultingConstraints.Count() > 0)
+                    {
+                        var condition = c.ToString();
+
+                        var certainty = res.FindMinimumCertainty();  // How do I ask SI(John) >= 40 and get its certainty
+
+                        total += certainty;
+                    }
+
                 }
+
+                var averageCertainty = total / totalSets;
+                totalCertainty += averageCertainty;
+            }
+       
 
                 toRet = totalCertainty / totalConds;
 
@@ -119,8 +136,7 @@ namespace CommeillFaut
 
             }
 
-            else return 0;
         }
 
     }
-}
+
