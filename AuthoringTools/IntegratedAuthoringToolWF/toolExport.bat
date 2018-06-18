c98aad7f8f6c7bc955b3c87b9aa3ca6782f25551
Manuel Guimaraes <manuel_guimaraes@hotmail.com>
2018-06-18 15:37:50 +0100

    Integrated Authoring tool is now correctly exported

--- AuthoringTools/IntegratedAuthoringToolWF/toolExport.bat
+++ AuthoringTools/IntegratedAuthoringToolWF/toolExport.bat
@@ -1,7 +1,7 @@
 @echo off
 
 SET origin=bin\Debug
-SET include=*.exe *.dll
+SET include=*.exe *.dll config6
 SET exclude=*vshost.exe *vshost.dll
 
 ROBOCOPY "%origin%" "%~f1" %include% /XF %exclude% /S /NP /NFL /NDL /NJH /NJS
