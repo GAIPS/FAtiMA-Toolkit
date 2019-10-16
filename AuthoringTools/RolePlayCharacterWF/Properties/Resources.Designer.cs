63f004281eb859b5086882d23480c17cb3c1e70d
Samuel <samuel.fm@gmail.com>
2019-10-16 14:50:44 +0100

    Fixed the Framework Targeting in the Authoring Tools Solution

--- AuthoringTools/RolePlayCharacterWF/Properties/Resources.Designer.cs
+++ AuthoringTools/RolePlayCharacterWF/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace RolePlayCharacterWF.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -86,7 +86,5 @@ namespace RolePlayCharacterWF.Properties {
                 return ResourceManager.GetString("UnableToSaveFileError", resourceCulture);
             }
         }
-
-        public static string EditEmotionFormTitle { get; internal set; }
     }
 }
