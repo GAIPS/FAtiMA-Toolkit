fe231e8318be7ac78af5f259d4c0d3962b09ba0c
Manuel Guimaraes <manuel_guimaraes@hotmail.com>
2022-06-13 19:14:14 +0100

    Story to Scenario Wizard now displays an example.

--- AuthoringTools/IntegratedAuthoringToolWF/IEP/computeDescriptionForm.Designer.cs
+++ AuthoringTools/IntegratedAuthoringToolWF/IEP/computeDescriptionForm.Designer.cs
@@ -125,10 +125,11 @@ namespace IntegratedAuthoringToolWF.IEP
             this.label6.Margin = new System.Windows.Forms.Padding(4);
             this.label6.Name = "label6";
             this.label6.Padding = new System.Windows.Forms.Padding(4);
-            this.label6.Size = new System.Drawing.Size(369, 71);
+            this.label6.Size = new System.Drawing.Size(393, 84);
             this.label6.TabIndex = 1;
             this.label6.Text = "1. Write a description of the intended scenario. \r\n2. Press the \"Compute Input\" b" +
-    "utton. \r\n3. Accept or Edit the resulting output\r\n\r\n\r\n";
+    "utton: \r\n       \"John loves Sarah. Sarah loves going to the cinema.\"\r\n3. Accept " +
+    "or Edit the resulting output\r\n\r\n\r\n";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // groupBox3
@@ -155,7 +156,7 @@ namespace IntegratedAuthoringToolWF.IEP
             | System.Windows.Forms.AnchorStyles.Right)));
             this.descriptionText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.descriptionText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.descriptionText.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.descriptionText.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.descriptionText.Location = new System.Drawing.Point(6, 19);
             this.descriptionText.Margin = new System.Windows.Forms.Padding(15, 16, 15, 16);
             this.descriptionText.Name = "descriptionText";
@@ -218,7 +219,6 @@ namespace IntegratedAuthoringToolWF.IEP
             // 
             // ComputeDescriptionForm
             // 
-            this.AcceptButton = this.computeStoryButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
